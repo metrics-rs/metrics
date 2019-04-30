7063b552fc1b985a8374e50050cd3b16a5f55bfd
Toby Lawrence <toby@nuclearfurnace.com>
2019-04-30 09:04:03 -0400

    Add documentation to the exporters/recorders facade modules.

--- metrics/src/exporters.rs
+++ metrics/src/exporters.rs
@@ -1,2 +1,5 @@
+//! Commonly used exporters.
+//!
+//! Exporters define where metric output goes: standard output, HTTP, etc.
 #[cfg(feature = "metrics-exporter-log")]
 pub use metrics_exporter_log::LogExporter;
--- metrics/src/recorders.rs
+++ metrics/src/recorders.rs
@@ -1,3 +1,6 @@
+//! Commonly used recorders.
+//!
+//! Recorders define the format of the metric output: text, JSON, etc.
 #[cfg(feature = "metrics-recorder-text")]
 pub use metrics_recorder_text::TextRecorder;
 
