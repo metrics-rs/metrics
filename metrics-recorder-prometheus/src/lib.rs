3aa728f3b05b9bbe50e2e73b66ca8c1b2a2ad932
Toby Lawrence <toby@nuclearfurnace.com>
2019-06-26 21:37:19 -0400

    recorder/prometheus: properly aggregate histograms + label support

--- metrics-recorder-prometheus/src/lib.rs
+++ metrics-recorder-prometheus/src/lib.rs
@@ -3,11 +3,13 @@
 use hdrhistogram::Histogram;
 use metrics_core::{Key, Recorder};
 use metrics_util::{parse_quantiles, Quantile};
+use std::collections::HashMap;
 use std::time::SystemTime;
 
 /// Records metrics in the Prometheus exposition format.
 pub struct PrometheusRecorder {
     quantiles: Vec<Quantile>,
+    histos: HashMap<Key, (u64, Histogram<u64>)>,
     output: String,
 }
 
@@ -30,6 +32,7 @@ impl PrometheusRecorder {
         let actual_quantiles = parse_quantiles(quantiles);
         Self {
             quantiles: actual_quantiles,
+            histos: HashMap::new(),
             output: get_prom_expo_header(),
         }
     }
@@ -37,57 +40,40 @@ impl PrometheusRecorder {
 
 impl Recorder for PrometheusRecorder {
     fn record_counter(&mut self, key: Key, value: u64) {
-        let label = key.name().replace('.', "_");
+        let (name, labels) = key_to_parts(&key);
+        let full_name = render_labeled_name(&name, &labels);
         self.output.push_str("\n# TYPE ");
-        self.output.push_str(label.as_str());
+        self.output.push_str(name.as_str());
         self.output.push_str(" counter\n");
-        self.output.push_str(label.as_str());
+        self.output.push_str(full_name.as_str());
         self.output.push_str(" ");
         self.output.push_str(value.to_string().as_str());
         self.output.push_str("\n");
     }
 
     fn record_gauge(&mut self, key: Key, value: i64) {
-        let label = key.name().replace('.', "_");
+        let (name, labels) = key_to_parts(&key);
+        let full_name = render_labeled_name(&name, &labels);
         self.output.push_str("\n# TYPE ");
-        self.output.push_str(label.as_str());
+        self.output.push_str(name.as_str());
         self.output.push_str(" gauge\n");
-        self.output.push_str(label.as_str());
+        self.output.push_str(full_name.as_str());
         self.output.push_str(" ");
         self.output.push_str(value.to_string().as_str());
         self.output.push_str("\n");
     }
 
     fn record_histogram(&mut self, key: Key, values: &[u64]) {
-        let mut sum = 0;
-        let mut h = Histogram::<u64>::new(3).expect("failed to create histogram");
+        let entry = self.histos.entry(key).or_insert_with(|| {
+            let h = Histogram::<u64>::new(3).expect("failed to create histogram");
+            (0, h)
+        });
+
+        let (sum, h) = entry;
         for value in values {
             h.record(*value).expect("failed to record histogram value");
-            sum += *value;
+            *sum += *value;
         }
-
-        let label = key.name().replace('.', "_");
-        self.output.push_str("\n# TYPE ");
-        self.output.push_str(label.as_str());
-        self.output.push_str(" summary\n");
-
-        for quantile in &self.quantiles {
-            let value = h.value_at_quantile(quantile.value());
-            self.output.push_str(label.as_str());
-            self.output.push_str("{quantile=\"");
-            self.output.push_str(quantile.value().to_string().as_str());
-            self.output.push_str("\"} ");
-            self.output.push_str(value.to_string().as_str());
-            self.output.push_str("\n");
-        }
-        self.output.push_str(label.as_str());
-        self.output.push_str("_sum ");
-        self.output.push_str(sum.to_string().as_str());
-        self.output.push_str("\n");
-        self.output.push_str(label.as_str());
-        self.output.push_str("_count ");
-        self.output.push_str(values.len().to_string().as_str());
-        self.output.push_str("\n");
     }
 }
 
@@ -95,6 +81,7 @@ impl Clone for PrometheusRecorder {
     fn clone(&self) -> Self {
         Self {
             output: get_prom_expo_header(),
+            histos: HashMap::new(),
             quantiles: self.quantiles.clone(),
         }
     }
@@ -102,8 +89,66 @@ impl Clone for PrometheusRecorder {
 
 impl Into<String> for PrometheusRecorder {
     fn into(self) -> String {
-        self.output
+        let mut output = self.output;
+
+        for (key, sh) in &self.histos {
+            let (sum, hist) = sh;
+            let (name, labels) = key_to_parts(&key);
+            output.push_str("\n# TYPE ");
+            output.push_str(name.as_str());
+            output.push_str(" summary\n");
+
+            for quantile in &self.quantiles {
+                let value = hist.value_at_quantile(quantile.value());
+                let mut labels = labels.clone();
+                labels.push(format!("quantile=\"{}\"", quantile.value()));
+                let full_name = render_labeled_name(&name, &labels);
+                output.push_str(full_name.as_str());
+                output.push_str(" ");
+                output.push_str(value.to_string().as_str());
+                output.push_str("\n");
+            }
+            let sum_name = format!("{}_sum", name);
+            let full_sum_name = render_labeled_name(&sum_name, &labels);
+            output.push_str(full_sum_name.as_str());
+            output.push_str(" ");
+            output.push_str(sum.to_string().as_str());
+            output.push_str("\n");
+            let count_name = format!("{}_count", name);
+            let full_count_name = render_labeled_name(&count_name, &labels);
+            output.push_str(full_count_name.as_str());
+            output.push_str(" ");
+            output.push_str(hist.len().to_string().as_str());
+            output.push_str("\n");
+        }
+
+        output
     }
+}
+
+fn key_to_parts(key: &Key) -> (String, Vec<String>) {
+    let name = key.name().replace('.', "_");
+    let labels = key
+        .labels()
+        .map(|labels| {
+            labels
+                .map(|l| format!("{}=\"{}\"", l.key(), l.value()))
+                .collect()
+        })
+        .unwrap_or_default();
+
+    (name, labels)
+}
+
+fn render_labeled_name(name: &str, labels: &[String]) -> String {
+    let mut output = name.to_string();
+    if !labels.is_empty() {
+        let joined = labels.join(",");
+        output.push_str("{");
+        output.push_str(&joined);
+        output.push_str("}");
+    }
+    output
 }
 
 fn get_prom_expo_header() -> String {
