7fcab1c46e4b5165a6bddea90fe67693b359e887
Jerome Gravel-Niquet <jeromegn@gmail.com>
2019-12-17 20:08:37 +0000

    Adds increment and decrement methods to Gauge (#64)
    * Adds increment and decrement methods to Gauge.

--- metrics-runtime/src/data/gauge.rs
+++ metrics-runtime/src/data/gauge.rs
@@ -13,6 +13,16 @@ impl Gauge {
     pub fn record(&self, value: i64) {
         self.handle.update_gauge(value);
     }
+
+    /// Increments the gauge's value
+    pub fn increment(&self, value: i64) {
+        self.handle.increment_gauge(value);
+    }
+
+    /// Decrements the gauge's value
+    pub fn decrement(&self, value: i64) {
+        self.handle.decrement_gauge(value);
+    }
 }
 
 impl From<ValueHandle> for Gauge {
