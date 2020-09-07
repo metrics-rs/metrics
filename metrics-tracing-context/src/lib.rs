d15f6477c22dc91c7f6b3f566d57c3bf692acdbb
Toby Lawrence <toby@nuclearfurnace.com>
2020-09-07 13:09:46 +0000

    Add ability to always include specific fields in the tracing layer.
    Tests currently fail because a decision has to be made about how to signal cacheability to the macro layer: either Recorder gets a new method that indicates whether or not keys can be cached, or we change Identifier to signal when it's cacheable or not.

--- metrics-tracing-context/src/lib.rs
+++ metrics-tracing-context/src/lib.rs
@@ -1,4 +1,4 @@
-use metrics::{Identifier, Key, Recorder};
+use metrics::{Identifier, Key, Label, Recorder};
 use metrics_util::layers::Layer;
 use tracing::Span;
 
@@ -6,18 +6,48 @@ mod tracing_integration;
 
 pub use tracing_integration::{MetricsLayer, SpanExt};
 
-pub struct TracingContextLayer;
+pub struct TracingContextLayer {
+    global_fields: Vec<String>,
+}
+
+impl TracingContextLayer {
+    /// Creates a new `TracingContextLayer` without any fields enabled globally.
+    ///
+    /// Only dynamic fields specified at the callsite will be resolved when using the layer in this mode.
+    pub fn new() -> Self {
+        Self {
+            global_fields: Vec::new(),
+        }
+    }
+
+    /// Creates a new `TracingContextLayer` with the specified fields enabled globally.
+    ///
+    /// This will inject the specified fields, if they are present in the current span, into all
+    /// metric label sets.  Fields are matched case-sensitively and in their entirety.
+    pub fn with_global_fields(global_fields: Vec<String>) -> Self {
+        Self {
+            global_fields,
+        }
+    }
+}
 
 impl<R> Layer<R> for TracingContextLayer {
     type Output = TracingContext<R>;
 
     fn layer(&self, inner: R) -> Self::Output {
-        TracingContext { inner }
+        TracingContext {
+            inner,
+            global_fields: self.global_fields.iter()
+                .cloned()
+                .map(Label::from_dynamic)
+                .collect(),
+        }
     }
 }
 
 pub struct TracingContext<R> {
     inner: R,
+    global_fields: Vec<Label>,
 }
 
 impl<R> TracingContext<R> {
@@ -26,6 +56,15 @@ impl<R> TracingContext<R> {
         span.with_labels(|new_labels| {
             if !new_labels.is_empty() {
                 let old_labels = key.labels_mut().get_or_insert_with(|| Vec::new());
+                if !self.global_fields.is_empty() {
+                    let global_fields = self.global_fields.clone();
+                    for global_field in global_fields {
+                        if old_labels.iter().position(|l| l.key() == global_field.key()).is_none() {
+                            old_labels.push(global_field);
+                        }
+                    }
+                }
+
                 for old_label in old_labels.iter_mut() {
                     // Check if this label still needs to be given a value.
                     if old_label.requires_value() {
@@ -33,6 +72,7 @@ impl<R> TracingContext<R> {
                         for new_label in new_labels.iter() {
                             if new_label.0 == old_label.key() {
                                 old_label.set_value(new_label.1.to_string());
+                                break;
                             }
                         }
                     }
--- metrics-tracing-context/tests/integration.rs
+++ metrics-tracing-context/tests/integration.rs
@@ -13,14 +13,16 @@ struct TestGuard {
     _tracing_guard: DefaultGuard,
 }
 
-fn setup() -> (TestGuard, Snapshotter) {
+fn setup(fields: Option<Vec<String>>) -> (TestGuard, Snapshotter) {
     let test_mutex_guard = TEST_MUTEX.lock();
     let subscriber = Registry::default().with(MetricsLayer::new());
     let tracing_guard = set_default(&Dispatch::new(subscriber));
 
     let recorder = DebuggingRecorder::new();
     let snapshotter = recorder.snapshotter();
-    let recorder = TracingContextLayer.layer(recorder);
+    let fields = fields.unwrap_or_else(|| Vec::new());
+    let tracing_layer = TracingContextLayer::with_global_fields(fields);
+    let recorder = tracing_layer.layer(recorder);
 
     metrics::clear_recorder();
     metrics::set_boxed_recorder(Box::new(recorder)).expect("failed to install recorder");
@@ -34,7 +36,7 @@ fn setup() -> (TestGuard, Snapshotter) {
 
 #[test]
 fn test_basic_functionality() {
-    let (_guard, snapshotter) = setup();
+    let (_guard, snapshotter) = setup(None);
 
     let user = "ferris";
     let email = "ferris@rust-lang.org";
@@ -64,7 +66,7 @@ fn test_basic_functionality() {
 
 #[test]
 fn test_no_labels() {
-    let (_guard, snapshotter) = setup();
+    let (_guard, snapshotter) = setup(None);
 
     let span = span!(Level::TRACE, "login");
     let _guard = span.enter();
@@ -85,7 +87,7 @@ fn test_no_labels() {
 
 #[test]
 fn test_multiple_paths_to_the_same_callsite() {
-    let (_guard, snapshotter) = setup();
+    let (_guard, snapshotter) = setup(None);
 
     let shared_fn = || {
         counter!("my_counter", 1, "shared_field", "dynamic_field");
@@ -142,6 +144,102 @@ fn test_multiple_paths_to_the_same_callsite() {
                     vec![
                         Label::from_dynamic_with_value("shared_field", "path2"),
                         Label::from_dynamic_with_value("dynamic_field", "bar_dynamic"),
+                    ],
+                ),
+                DebugValue::Counter(1),
+            )
+        ]
+    )
+}
+
+#[test]
+fn test_global_fields() {
+    let fields = vec![String::from("common"), String::from("rare")];
+    let (_guard, snapshotter) = setup(Some(fields));
+
+    let shared_fn1 = || {
+        counter!("my_counter", 1, "common");
+    };
+
+    let shared_fn2 = || {
+        counter!("my_counter", 1);
+    };
+
+    let common1 = || {
+        let span = span!(Level::TRACE, "span1", common = "common1");
+        let _guard = span.enter();
+        shared_fn1();
+    };
+
+    let common2 = || {
+        let span = span!(Level::TRACE, "span2", common = "common2", rare = "rare2");
+        let _guard = span.enter();
+        shared_fn1();
+    };
+
+    let common3 = || {
+        let span = span!(Level::TRACE, "span3", common = "common3");
+        let _guard = span.enter();
+        shared_fn2();
+    };
+
+    let common4 = || {
+        let span = span!(Level::TRACE, "span4", common = "common4", rare = "rare4");
+        let _guard = span.enter();
+        shared_fn2();
+    };
+
+    common1();
+    common2();
+    common3();
+    common4();
+
+    let mut snapshot = snapshotter.snapshot();
+    snapshot.sort();
+
+    assert_eq!(
+        snapshot,
+        vec![
+            (
+                MetricKind::Counter,
+                Key::from_name_and_labels(
+                    "my_counter",
+                    vec![
+                        Label::from_dynamic_with_value("common", "common1"),
+                        Label::from_dynamic("rare"),
+                    ],
+                ),
+                DebugValue::Counter(1),
+            ),
+            (
+                MetricKind::Counter,
+                Key::from_name_and_labels(
+                    "my_counter",
+                    vec![
+                        Label::from_dynamic_with_value("common", "common2"),
+                        Label::from_dynamic_with_value("rare", "rare2"),
+                    ],
+                ),
+                DebugValue::Counter(1),
+            ),
+            (
+                MetricKind::Counter,
+                Key::from_name_and_labels(
+                    "my_counter",
+                    vec![
+                        Label::from_dynamic_with_value("common", "common3"),
+                        Label::from_dynamic("rare"),
+                    ],
+                ),
+                DebugValue::Counter(1),
+            ),
+            (
+                MetricKind::Counter,
+                Key::from_name_and_labels(
+                    "my_counter",
+                    vec![
+                        Label::from_dynamic_with_value("common", "common4"),
+                        Label::from_dynamic_with_value("rare", "rare4"),
                     ],
                 ),
                 DebugValue::Counter(1),
