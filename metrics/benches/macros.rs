48d6af8897d94614f6dc622b43dcab9ccc7b46cb
MOZGIII <mike-n@narod.ru>
2020-09-10 20:17:17 +0300

    Apply workaround for criterion comparison

--- metrics/benches/macros.rs
+++ metrics/benches/macros.rs
@@ -31,31 +31,31 @@ fn reset_recorder() {
 fn macro_benchmark(c: &mut Criterion) {
     c.bench(
         "macros",
-        Benchmark::new("uninitialized/no labels", |b| {
+        Benchmark::new("uninitialized/no_labels", |b| {
             b.iter(|| {
                 counter!("counter_bench", 42);
             })
         })
-        .with_function("uninitialized/with static labels", |b| {
+        .with_function("uninitialized/with_static_labels", |b| {
             b.iter(|| {
                 counter!("counter_bench", 42, "request" => "http", "svc" => "admin");
             })
         })
-        .with_function("initialized/no labels", |b| {
+        .with_function("initialized/no_labels", |b| {
             reset_recorder();
             b.iter(|| {
                 counter!("counter_bench", 42);
             });
             metrics::clear_recorder();
         })
-        .with_function("initialized/with static labels", |b| {
+        .with_function("initialized/with_static_labels", |b| {
             reset_recorder();
             b.iter(|| {
                 counter!("counter_bench", 42, "request" => "http", "svc" => "admin");
             });
             metrics::clear_recorder();
         })
-        .with_function("initialized/with dynamic labels", |b| {
+        .with_function("initialized/with_dynamic_labels", |b| {
             let label_val = thread_rng().gen::<u64>().to_string();
 
             reset_recorder();
