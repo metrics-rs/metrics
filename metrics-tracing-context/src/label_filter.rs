406f5155c30210fa13b2a1431b1155168608b091
MOZGIII <mike-n@narod.ru>
2020-09-15 14:38:39 +0300

    Update metrics-tracing-context/src/label_filter.rs
    Co-authored-by: Toby Lawrence <tobz@users.noreply.github.com>

--- metrics-tracing-context/src/label_filter.rs
+++ metrics-tracing-context/src/label_filter.rs
@@ -3,7 +3,7 @@
 use metrics::Label;
 
 /// [`LabelFilter`] trait encapsulates the ability to filter labels, i.e.
-/// determinig whether a particular label should be included in the key or not.
+/// determining whether a particular span field should be included as a label or not.
 pub trait LabelFilter {
     /// Returns `true` if the passed label should be included in the key.
     fn should_include_label(&self, label: &Label) -> bool;
