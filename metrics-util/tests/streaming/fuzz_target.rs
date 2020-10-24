a2955973d789648d77a0bbd4c6b706cfb2211326
Toby Lawrence <toby@nuclearfurnace.com>
2020-10-24 11:18:07 -0400

    remove StreamingIntegers since we don't use it anymore

--- metrics-util/tests/streaming/fuzz_target.rs
+++
@@ -1,9 +0,0 @@
-use bolero::fuzz;
-use metrics_util::StreamingIntegers;
-
-fn main() {
-    fuzz!().with_type().for_each(|value: &Vec<u64>| {
-        let mut si = StreamingIntegers::new();
-        si.compress(&value);
-    });
-}
