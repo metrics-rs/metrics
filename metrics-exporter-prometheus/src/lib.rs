01fcc020c743816eed397a476c85455c86ca4fd5
str4d <thestr4d@gmail.com>
2020-11-11 15:34:30 +0000

    PrometheusBuilder::install: Build exporter in runtime context
    Closes https://github.com/metrics-rs/metrics/issues/122

--- metrics-exporter-prometheus/src/lib.rs
+++ metrics-exporter-prometheus/src/lib.rs
@@ -443,13 +443,13 @@ impl PrometheusBuilder {
     /// installing the recorder as the global recorder.
     #[cfg(feature = "tokio-exporter")]
     pub fn install(self) -> Result<(), Error> {
-        let (recorder, exporter) = self.build_with_exporter()?;
-        metrics::set_boxed_recorder(Box::new(recorder))?;
-
         let mut runtime = runtime::Builder::new()
             .basic_scheduler()
             .enable_all()
             .build()?;
+
+        let (recorder, exporter) = runtime.enter(|| self.build_with_exporter())?;
+        metrics::set_boxed_recorder(Box::new(recorder))?;
 
         thread::Builder::new()
             .name("metrics-exporter-prometheus-http".to_string())
