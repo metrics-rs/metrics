a10a17b88155c90a10ac4927a80e356e078cfa46
MOZGIII <mike-n@narod.ru>
2020-12-28 18:24:31 +0300

    Upgrade criterion invocations to the modern form

--- metrics/benches/macros.rs
+++ metrics/benches/macros.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::{Benchmark, Criterion};
+use criterion::Criterion;
 
 use metrics::{counter, GaugeValue, Key, Recorder, Unit};
 use rand::{thread_rng, Rng};
@@ -30,44 +30,43 @@ fn reset_recorder() {
 }
 
 fn macro_benchmark(c: &mut Criterion) {
-    c.bench(
-        "macros",
-        Benchmark::new("uninitialized/no_labels", |b| {
-            metrics::clear_recorder();
-            b.iter(|| {
-                counter!("counter_bench", 42);
-            })
+    let mut group = c.benchmark_group("macros");
+    group.bench_function("uninitialized/no_labels", |b| {
+        metrics::clear_recorder();
+        b.iter(|| {
+            counter!("counter_bench", 42);
         })
-        .with_function("uninitialized/with_static_labels", |b| {
-            metrics::clear_recorder();
-            b.iter(|| {
-                counter!("counter_bench", 42, "request" => "http", "svc" => "admin");
-            })
+    });
+    group.bench_function("uninitialized/with_static_labels", |b| {
+        metrics::clear_recorder();
+        b.iter(|| {
+            counter!("counter_bench", 42, "request" => "http", "svc" => "admin");
         })
-        .with_function("initialized/no_labels", |b| {
-            reset_recorder();
-            b.iter(|| {
-                counter!("counter_bench", 42);
-            });
-            metrics::clear_recorder();
-        })
-        .with_function("initialized/with_static_labels", |b| {
-            reset_recorder();
-            b.iter(|| {
-                counter!("counter_bench", 42, "request" => "http", "svc" => "admin");
-            });
-            metrics::clear_recorder();
-        })
-        .with_function("initialized/with_dynamic_labels", |b| {
-            let label_val = thread_rng().gen::<u64>().to_string();
+    });
+    group.bench_function("initialized/no_labels", |b| {
+        reset_recorder();
+        b.iter(|| {
+            counter!("counter_bench", 42);
+        });
+        metrics::clear_recorder();
+    });
+    group.bench_function("initialized/with_static_labels", |b| {
+        reset_recorder();
+        b.iter(|| {
+            counter!("counter_bench", 42, "request" => "http", "svc" => "admin");
+        });
+        metrics::clear_recorder();
+    });
+    group.bench_function("initialized/with_dynamic_labels", |b| {
+        let label_val = thread_rng().gen::<u64>().to_string();
 
-            reset_recorder();
-            b.iter(move || {
-                counter!("counter_bench", 42, "request" => "http", "uid" => label_val.clone());
-            });
-            metrics::clear_recorder();
-        }),
-    );
+        reset_recorder();
+        b.iter(move || {
+            counter!("counter_bench", 42, "request" => "http", "uid" => label_val.clone());
+        });
+        metrics::clear_recorder();
+    });
+    group.finish();
 }
 
 criterion_group!(benches, macro_benchmark);
