1a2f3d94c2a0016d1ba3e78aede72eda9c3a41b5
MOZGIII <mike-n@narod.ru>
2020-12-28 20:21:43 +0300

    Add more benches

--- metrics-tracing-context/benches/layer.rs
+++ metrics-tracing-context/benches/layer.rs
@@ -20,6 +20,42 @@ fn layer_benchmark(c: &mut Criterion) {
             recorder.increment_counter(Key::Borrowed(&KEY_DATA), 1);
         })
     });
+    group.bench_function("layer overhead", |b| {
+        let recorder = passthrough::Layer {
+            inner: NoopRecorder,
+        };
+        static KEY_NAME: [SharedString; 1] = [SharedString::const_str("key")];
+        static KEY_LABELS: [Label; 1] = [Label::from_static_parts("foo", "bar")];
+        static KEY_DATA: KeyData = KeyData::from_static_parts(&KEY_NAME, &KEY_LABELS);
+
+        b.iter(|| {
+            recorder.increment_counter(Key::Borrowed(&KEY_DATA), 1);
+        })
+    });
+    group.bench_function("reassambly overhead", |b| {
+        let recorder = reassambly::Layer {
+            inner: NoopRecorder,
+        };
+        static KEY_NAME: [SharedString; 1] = [SharedString::const_str("key")];
+        static KEY_LABELS: [Label; 1] = [Label::from_static_parts("foo", "bar")];
+        static KEY_DATA: KeyData = KeyData::from_static_parts(&KEY_NAME, &KEY_LABELS);
+
+        b.iter(|| {
+            recorder.increment_counter(Key::Borrowed(&KEY_DATA), 1);
+        })
+    });
+    group.bench_function("enhancement overhead", |b| {
+        let recorder = enhancer::Layer {
+            inner: NoopRecorder,
+        };
+        static KEY_NAME: [SharedString; 1] = [SharedString::const_str("key")];
+        static KEY_LABELS: [Label; 1] = [Label::from_static_parts("foo", "bar")];
+        static KEY_DATA: KeyData = KeyData::from_static_parts(&KEY_NAME, &KEY_LABELS);
+
+        b.iter(|| {
+            recorder.increment_counter(Key::Borrowed(&KEY_DATA), 1);
+        })
+    });
     group.bench_function("no integration", |b| {
         let subscriber = Registry::default();
         let dispatcher = Dispatch::new(subscriber);
@@ -103,3 +139,180 @@ fn layer_benchmark(c: &mut Criterion) {
 
 criterion_group!(benches, layer_benchmark);
 criterion_main!(benches);
+
+/// A simple passthrough layer.
+/// Allows us to estimate the penalty of the layer.
+mod passthrough {
+    use super::*;
+    use metrics::{GaugeValue, Unit};
+
+    pub struct Layer<R> {
+        pub inner: R,
+    }
+
+    impl<R> Recorder for Layer<R>
+    where
+        R: Recorder,
+    {
+        fn register_counter(
+            &self,
+            key: Key,
+            unit: Option<Unit>,
+            description: Option<&'static str>,
+        ) {
+            self.inner.register_counter(key, unit, description)
+        }
+
+        fn register_gauge(&self, key: Key, unit: Option<Unit>, description: Option<&'static str>) {
+            self.inner.register_gauge(key, unit, description)
+        }
+
+        fn register_histogram(
+            &self,
+            key: Key,
+            unit: Option<Unit>,
+            description: Option<&'static str>,
+        ) {
+            self.inner.register_histogram(key, unit, description)
+        }
+
+        fn increment_counter(&self, key: Key, value: u64) {
+            self.inner.increment_counter(key, value);
+        }
+
+        fn update_gauge(&self, key: Key, value: GaugeValue) {
+            self.inner.update_gauge(key, value);
+        }
+
+        fn record_histogram(&self, key: Key, value: f64) {
+            self.inner.record_histogram(key, value);
+        }
+    }
+}
+
+/// A layer with a dummy layer implementation that only reassambles the Key.
+/// Allows us to estimate the penalty of the said reassambly.
+mod reassambly {
+    use super::*;
+    use metrics::{GaugeValue, Unit};
+
+    pub struct Layer<R> {
+        pub inner: R,
+    }
+
+    impl<R> Layer<R> {
+        fn enhance_key(&self, key: Key) -> Key {
+            let (name, labels) = key.into_owned().into_parts();
+            // nothing here, just reassembly
+            KeyData::from_parts(name, labels).into()
+        }
+    }
+
+    impl<R> Recorder for Layer<R>
+    where
+        R: Recorder,
+    {
+        fn register_counter(
+            &self,
+            key: Key,
+            unit: Option<Unit>,
+            description: Option<&'static str>,
+        ) {
+            self.inner.register_counter(key, unit, description)
+        }
+
+        fn register_gauge(&self, key: Key, unit: Option<Unit>, description: Option<&'static str>) {
+            self.inner.register_gauge(key, unit, description)
+        }
+
+        fn register_histogram(
+            &self,
+            key: Key,
+            unit: Option<Unit>,
+            description: Option<&'static str>,
+        ) {
+            self.inner.register_histogram(key, unit, description)
+        }
+
+        fn increment_counter(&self, key: Key, value: u64) {
+            let key = self.enhance_key(key);
+            self.inner.increment_counter(key, value);
+        }
+
+        fn update_gauge(&self, key: Key, value: GaugeValue) {
+            let key = self.enhance_key(key);
+            self.inner.update_gauge(key, value);
+        }
+
+        fn record_histogram(&self, key: Key, value: f64) {
+            let key = self.enhance_key(key);
+            self.inner.record_histogram(key, value);
+        }
+    }
+}
+
+/// A simple layer that adds an owned string layer.
+/// Allows us to estimate the penalty of a layer with this operation.
+mod enhancer {
+    use super::*;
+    use metrics::{GaugeValue, Unit};
+
+    pub struct Layer<R> {
+        pub inner: R,
+    }
+
+    impl<R> Layer<R> {
+        fn enhance_labels(&self, labels: &mut Vec<Label>) {
+            // Key is static, value is not.
+            labels.push(Label::new("mykey", "myval".to_owned()));
+        }
+
+        fn enhance_key(&self, key: Key) -> Key {
+            let (name, mut labels) = key.into_owned().into_parts();
+            self.enhance_labels(&mut labels);
+            KeyData::from_parts(name, labels).into()
+        }
+    }
+
+    impl<R> Recorder for Layer<R>
+    where
+        R: Recorder,
+    {
+        fn register_counter(
+            &self,
+            key: Key,
+            unit: Option<Unit>,
+            description: Option<&'static str>,
+        ) {
+            self.inner.register_counter(key, unit, description)
+        }
+
+        fn register_gauge(&self, key: Key, unit: Option<Unit>, description: Option<&'static str>) {
+            self.inner.register_gauge(key, unit, description)
+        }
+
+        fn register_histogram(
+            &self,
+            key: Key,
+            unit: Option<Unit>,
+            description: Option<&'static str>,
+        ) {
+            self.inner.register_histogram(key, unit, description)
+        }
+
+        fn increment_counter(&self, key: Key, value: u64) {
+            let key = self.enhance_key(key);
+            self.inner.increment_counter(key, value);
+        }
+
+        fn update_gauge(&self, key: Key, value: GaugeValue) {
+            let key = self.enhance_key(key);
+            self.inner.update_gauge(key, value);
+        }
+
+        fn record_histogram(&self, key: Key, value: f64) {
+            let key = self.enhance_key(key);
+            self.inner.record_histogram(key, value);
+        }
+    }
+}
