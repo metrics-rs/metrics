1a30029e36ea72d583b52773795796348da10111
Toby Lawrence <tobz@users.noreply.github.com>
2022-02-20 16:24:57 -0500

    fixed a glitch with Recency not actually properly tracking generations? (#276)
    Signed-off-by: Toby Lawrence <toby@nuclearfurnace.com>

--- metrics-util/src/recency.rs
+++ metrics-util/src/recency.rs
@@ -298,6 +298,7 @@ impl Recency {
                     } else {
                         // Value has changed, so mark it such.
                         *last_update = now;
+                        *last_gen = gen;
                     }
                 } else {
                     entries.insert(key.clone(), (gen, now));
