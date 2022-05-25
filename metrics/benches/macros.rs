f5dc231d4866e480be2f0edc866ba8cf9910cd8f
Toby Lawrence <toby@nuclearfurnace.com>
2022-05-25 07:56:54 -0400

    Fix benchmark.
    Signed-off-by: Toby Lawrence <toby@nuclearfurnace.com>

--- metrics/benches/macros.rs
+++ metrics/benches/macros.rs
@@ -24,20 +24,24 @@ impl Recorder for TestRecorder {
 }
 
 fn reset_recorder() {
-    let recorder = unsafe { &*Box::into_raw(Box::new(TestRecorder::default())) };
+    let recorder = Box::leak(Box::new(TestRecorder::default()));
     unsafe { metrics::set_recorder_racy(recorder).unwrap() }
 }
 
 fn macro_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("macros");
     group.bench_function("uninitialized/no_labels", |b| {
-        metrics::clear_recorder();
+        unsafe {
+            metrics::clear_recorder();
+        }
         b.iter(|| {
             counter!("counter_bench", 42);
         })
     });
     group.bench_function("uninitialized/with_static_labels", |b| {
-        metrics::clear_recorder();
+        unsafe {
+            metrics::clear_recorder();
+        }
         b.iter(|| {
             counter!("counter_bench", 42, "request" => "http", "svc" => "admin");
         })
@@ -47,14 +51,18 @@ fn macro_benchmark(c: &mut Criterion) {
         b.iter(|| {
             counter!("counter_bench", 42);
         });
-        metrics::clear_recorder();
+        unsafe {
+            metrics::clear_recorder();
+        }
     });
     group.bench_function("initialized/with_static_labels", |b| {
         reset_recorder();
         b.iter(|| {
             counter!("counter_bench", 42, "request" => "http", "svc" => "admin");
         });
-        metrics::clear_recorder();
+        unsafe {
+            metrics::clear_recorder();
+        }
     });
     group.bench_function("initialized/with_dynamic_labels", |b| {
         let label_val = thread_rng().gen::<u64>().to_string();
@@ -63,7 +71,9 @@ fn macro_benchmark(c: &mut Criterion) {
         b.iter(move || {
             counter!("counter_bench", 42, "request" => "http", "uid" => label_val.clone());
         });
-        metrics::clear_recorder();
+        unsafe {
+            metrics::clear_recorder();
+        }
     });
     group.finish();
 }
