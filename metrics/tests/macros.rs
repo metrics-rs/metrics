aee9a2a3d8151c414fc57db440f0e5300b517022
CinchBlue <aytasato@gmail.com>
2023-04-13 16:52:06 -0700

    Use global paths for macros (#358)

--- metrics/tests/macros.rs
+++ metrics/tests/macros.rs
@@ -3,4 +3,5 @@ pub fn macros() {
     let t = trybuild::TestCases::new();
     t.pass("tests/macros/01_basic.rs");
     t.pass("tests/macros/02_trailing_comma.rs");
+    t.pass("tests/macros/03_mod_aliasing.rs");
 }
