a83f591bff575617a1a3c22d2d791adb08ccf436
Bianca Pelegrini <57107170+biancamip@users.noreply.github.com>
2024-01-14 12:32:54 -0300

    Specific endpoint for prometheus exporter health checking (#435)

--- metrics-exporter-prometheus/src/builder.rs
+++ metrics-exporter-prometheus/src/builder.rs
@@ -435,13 +435,15 @@ impl PrometheusBuilder {
                         let handle = handle.clone();
 
                         async move {
-                            Ok::<_, hyper::Error>(service_fn(move |_| {
+                            Ok::<_, hyper::Error>(service_fn(move |req| {
                                 let handle = handle.clone();
 
                                 async move {
                                     if is_allowed {
-                                        let output = handle.render();
-                                        Ok::<_, hyper::Error>(Response::new(Body::from(output)))
+                                        Ok::<_, hyper::Error>(match req.uri().path() {
+                                            "/health" => Response::new(Body::from("OK")),
+                                            _ => Response::new(Body::from(handle.render())),
+                                        })
                                     } else {
                                         Ok::<_, hyper::Error>(
                                             Response::builder()
