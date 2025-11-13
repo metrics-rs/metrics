cf28ce9c8c562a49c30beb22371a4a94e51f1432
tinyfoolish <tinyfoolish@outlook.com>
2025-11-13 12:41:21 +0800

    docs: minor improvement for docs
    Signed-off-by: tinyfoolish <tinyfoolish@outlook.com>

--- metrics/src/cow.rs
+++ metrics/src/cow.rs
@@ -78,8 +78,8 @@ enum Kind {
 ///
 /// Up until this point, it might not be clear why we didn't just use `beef`. In truth, our design
 /// is fundamentally based on `beef`. Crucially, however, `beef` did not/still does not support
-/// `const` construction for generic slices.  Remember how we mentioned labels? The labels of a
-/// metric `are `[Label]` under-the-hood, and so without a way to construct them in a `const`
+/// `const` construction for generic slices. Remember how we mentioned labels? The labels of a
+/// metric are `[Label]` under-the-hood, and so without a way to construct them in a `const`
 /// fashion, our previous work to allow entirely static keys would not be possible.
 ///
 /// Thus, we forked `beef` and copied into directly into `metrics` so that we could write a
